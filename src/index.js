82ff15a53d5ab73248a2063c11e9c4fd87969a91
Anna Kolesnikova <ani.kolesnikova@gmail.com>
2018-09-11 17:51:06 +0300

    added numbers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-module.exports = function solveEquation(equation) {};
+module.exports = function solveEquation(equation) {
+  let array = equation.split(" ");
+  let a = parseInt(array[0]);
+  let b = array[4] + perseInt(array[3] + 1);
+  let c = array[8] + parseInt(array[7] + 1);
+  let desc = sqrt(b) - 4 * a * c;
+};
